08b675f36b97a25fdbaf4359d3fe4a3fe5606b19
Sebe112 <92109053+Sebe112@users.noreply.github.com>
2025-02-05 10:36:37 +0100

    Update BattleRepository.cs

--- Samurai.DAL/Repositories/BattleRepository.cs
+++ Samurai.DAL/Repositories/BattleRepository.cs
@@ -1,14 +1,55 @@
 using Samurai.DAL.Interfaces;
 using Samurai.DAL.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Samurai.DAL.Repositories
 {
     public class BattleRepository : ISamurai
     {
-        DatabaseContext context { get; set; }
+        private readonly DatabaseContext context;
+
         public BattleRepository(DatabaseContext c)
         {
             context = c;
+        }
+
+        public async Task<List<Battle>> GetAllBattlesAsync()
+        {
+            return await context.Battles.ToListAsync();
+        }
+
+        public async Task<Battle?> GetBattleByIdAsync(int id)
+        {
+            return await context.Battles.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task AddBattleAsync(Battle battle)
+        {
+            context.Battles.Add(battle);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBattleAsync(Battle battle)
+        {
+            var existingBattle = await context.Battles.FindAsync(battle.Id);
+            if (existingBattle != null)
+            {
+                existingBattle.Name = battle.Name;
+                existingBattle.StartDate = battle.StartDate;
+                existingBattle.EndDate = battle.EndDate;
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteBattleAsync(int id)
+        {
+            var battle = await context.Battles.FindAsync(id);
+            if (battle != null)
+            {
+                context.Battles.Remove(battle);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
