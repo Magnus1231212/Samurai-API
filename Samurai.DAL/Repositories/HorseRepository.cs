b4b5bb49c8f6ed2250d9282f7ec4cb49be1eb40a
MrSa2660 <Salih2660akkaymak@gmail.com>
2025-02-05 10:45:37 +0100

    CRUD Horse

--- Samurai.DAL/Repositories/HorseRepository.cs
+++ Samurai.DAL/Repositories/HorseRepository.cs
@@ -1,14 +1,44 @@
 using Samurai.DAL.Interfaces;
 using Samurai.DAL.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Samurai.DAL.Repositories
 {
     public class HorseRepository : ISamurai
     {
-        DatabaseContext context { get; set; }
-        public HorseRepository(DatabaseContext c)
+        private readonly DatabaseContext context;
+        
+        public HorseRepository(DatabaseContext context)
         {
-            context = c;
+            this.context = context;
         }
-    }
+
+        public async Task AddHorseAsync(Horse horse)
+        {
+            await context.Horses.AddAsync(horse);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<Horse>> GetAllHorsesAsync()
+        {
+            return await context.Horses.ToListAsync();
+        }
+
+        public async Task<Horse?> GetHorseByIdAsync(int id)
+        {
+            return await context.Horses.FirstOrDefaultAsync(h => h.Id == id);
+        }
+
+        public async Task UpdateHorseAsync(Horse horse)
+        {
+            context.Horses.Update(horse);
+            await context.SaveChangesAsync();
+        }
+        
+        public async Task DeleteHorseAsync(Horse horse)
+        {
+            context.Horses.Remove(await GetHorseByIdAsync(horse.Id));
+            await context.SaveChangesAsync();
+        }
+    }  
 }
