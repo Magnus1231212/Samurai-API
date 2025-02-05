b810e40a6212b63202be24bd237009c15f04a0c9
Sebe112 <92109053+Sebe112@users.noreply.github.com>
2025-02-05 12:15:37 +0100

    XML 👎

--- Samurai.API/Controllers/SamuraiController.cs
+++ Samurai.API/Controllers/SamuraiController.cs
@@ -1,102 +1,153 @@
 using Microsoft.AspNetCore.Mvc;
 using Samurai.DAL.Interfaces;
 using Samurai.DAL.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-[ApiController]
-[Route("api/[controller]")]
-public class SamuraiController : ControllerBase
+namespace Samurai.API.Controllers
 {
-    private readonly ISamurai _samuraiRepository;
-
-    public SamuraiController(ISamurai samuraiRepository)
+    /// <summary>
+    /// API Controller for managing Samurai entities.
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SamuraiController : ControllerBase
     {
-        _samuraiRepository = samuraiRepository;
-    }
+        private readonly ISamurai _samuraiRepository;
 
-    // GET: api/Samurai
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<SamuraiModel>>> GetSamurais()
-    {
-        var samurais = await _samuraiRepository.GetAllSamuraisAsync();
-        return Ok(samurais);
-    }
-
-    // GET: api/Samurai/5
-    [HttpGet("{id}")]
-    public async Task<ActionResult<SamuraiModel>> GetSamurai(int id)
-    {
-        var samurai = await _samuraiRepository.GetSamuraiAsync(id);
-        if (samurai == null)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SamuraiController"/> class.
+        /// </summary>
+        /// <param name="samuraiRepository">The samurai repository instance.</param>
+        public SamuraiController(ISamurai samuraiRepository)
         {
-            return NotFound();
-        }
-        return Ok(samurai);
-    }
-
-    // POST: api/Samurai
-    [HttpPost]
-    public async Task<ActionResult<SamuraiModel>> PostSamurai(SamuraiModel samurai)
-    {
-        await _samuraiRepository.AddSamuraiAsync(samurai);
-        return CreatedAtAction(nameof(GetSamurai), new { id = samurai.Id }, samurai);
-    }
-
-    // PUT: api/Samurai/5
-    [HttpPut("{id}")]
-    public async Task<IActionResult> PutSamurai(int id, SamuraiModel samurai)
-    {
-        if (id != samurai.Id)
-        {
-            return BadRequest();
+            _samuraiRepository = samuraiRepository;
         }
 
-        await _samuraiRepository.UpdateSamuraiAsync(samurai);
-        return NoContent();
-    }
-
-    // DELETE: api/Samurai/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteSamurai(int id)
-    {
-        var samurai = await _samuraiRepository.GetSamuraiAsync(id);
-        if (samurai == null)
+        /// <summary>
+        /// Retrieves all samurais.
+        /// </summary>
+        /// <returns>A list of all samurais.</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SamuraiModel>>> GetSamurais()
         {
-            return NotFound();
+            var samurais = await _samuraiRepository.GetAllSamuraisAsync();
+            return Ok(samurais);
         }
 
-        await _samuraiRepository.DeleteSamuraiAsync(id);
-        return NoContent();
-    }
+        /// <summary>
+        /// Retrieves a specific samurai by ID.
+        /// </summary>
+        /// <param name="id">The ID of the samurai to retrieve.</param>
+        /// <returns>The samurai entity if found; otherwise, NotFound.</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SamuraiModel>> GetSamurai(int id)
+        {
+            var samurai = await _samuraiRepository.GetSamuraiAsync(id);
+            if (samurai == null)
+            {
+                return NotFound();
+            }
+            return Ok(samurai);
+        }
 
-    // PUT: api/Samurai/addBattle/5/1
-    [HttpPut("addBattle/{samuraiId}/{battleId}")]
-    public async Task<IActionResult> AddBattleToSamurai(int samuraiId, int battleId)
-    {
-        await _samuraiRepository.AddBattleToSamuraiAsync(samuraiId, battleId);
-        return NoContent();
-    }
+        /// <summary>
+        /// Adds a new samurai.
+        /// </summary>
+        /// <param name="samurai">The samurai entity to add.</param>
+        /// <returns>The created samurai entity.</returns>
+        [HttpPost]
+        public async Task<ActionResult<SamuraiModel>> PostSamurai(SamuraiModel samurai)
+        {
+            await _samuraiRepository.AddSamuraiAsync(samurai);
+            return CreatedAtAction(nameof(GetSamurai), new { id = samurai.Id }, samurai);
+        }
 
-    // DELETE: api/Samurai/removeBattle/5/1
-    [HttpDelete("removeBattle/{samuraiId}/{battleId}")]
-    public async Task<IActionResult> RemoveBattleFromSamurai(int samuraiId, int battleId)
-    {
-        await _samuraiRepository.RemoveBattleFromSamuraiAsync(samuraiId, battleId);
-        return NoContent();
-    }
+        /// <summary>
+        /// Updates an existing samurai.
+        /// </summary>
+        /// <param name="id">The ID of the samurai to update.</param>
+        /// <param name="samurai">The updated samurai entity.</param>
+        /// <returns>No content if the update is successful; otherwise, BadRequest if the ID does not match.</returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSamurai(int id, SamuraiModel samurai)
+        {
+            if (id != samurai.Id)
+            {
+                return BadRequest();
+            }
 
-    // PUT: api/Samurai/addHorse/5/1
-    [HttpPut("addHorse/{samuraiId}/{horseId}")]
-    public async Task<IActionResult> AddHorseToSamurai(int samuraiId, int horseId)
-    {
-        await _samuraiRepository.AddHorseToSamuraiAsync(samuraiId, horseId);
-        return NoContent();
-    }
+            await _samuraiRepository.UpdateSamuraiAsync(samurai);
+            return NoContent();
+        }
 
-    // DELETE: api/Samurai/removeHorse/5/
-    [HttpDelete("removeHorse/{samuraiId}")]
-    public async Task<IActionResult> RemoveHorseFromSamurai(int samuraiId)
-    {
-        await _samuraiRepository.RemoveHorseFromSamuraiAsync(samuraiId);
-        return NoContent();
+        /// <summary>
+        /// Deletes a samurai by ID.
+        /// </summary>
+        /// <param name="id">The ID of the samurai to delete.</param>
+        /// <returns>No content if the deletion is successful; otherwise, NotFound if the samurai does not exist.</returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSamurai(int id)
+        {
+            var samurai = await _samuraiRepository.GetSamuraiAsync(id);
+            if (samurai == null)
+            {
+                return NotFound();
+            }
+
+            await _samuraiRepository.DeleteSamuraiAsync(id);
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Adds a battle to a samurai.
+        /// </summary>
+        /// <param name="samuraiId">The ID of the samurai.</param>
+        /// <param name="battleId">The ID of the battle to add.</param>
+        /// <returns>No content if the operation is successful.</returns>
+        [HttpPut("addBattle/{samuraiId}/{battleId}")]
+        public async Task<IActionResult> AddBattleToSamurai(int samuraiId, int battleId)
+        {
+            await _samuraiRepository.AddBattleToSamuraiAsync(samuraiId, battleId);
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Removes a battle from a samurai.
+        /// </summary>
+        /// <param name="samuraiId">The ID of the samurai.</param>
+        /// <param name="battleId">The ID of the battle to remove.</param>
+        /// <returns>No content if the operation is successful.</returns>
+        [HttpDelete("removeBattle/{samuraiId}/{battleId}")]
+        public async Task<IActionResult> RemoveBattleFromSamurai(int samuraiId, int battleId)
+        {
+            await _samuraiRepository.RemoveBattleFromSamuraiAsync(samuraiId, battleId);
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Adds a horse to a samurai.
+        /// </summary>
+        /// <param name="samuraiId">The ID of the samurai.</param>
+        /// <param name="horseId">The ID of the horse to add.</param>
+        /// <returns>No content if the operation is successful.</returns>
+        [HttpPut("addHorse/{samuraiId}/{horseId}")]
+        public async Task<IActionResult> AddHorseToSamurai(int samuraiId, int horseId)
+        {
+            await _samuraiRepository.AddHorseToSamuraiAsync(samuraiId, horseId);
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Removes the horse from a samurai.
+        /// </summary>
+        /// <param name="samuraiId">The ID of the samurai.</param>
+        /// <returns>No content if the operation is successful.</returns>
+        [HttpDelete("removeHorse/{samuraiId}")]
+        public async Task<IActionResult> RemoveHorseFromSamurai(int samuraiId)
+        {
+            await _samuraiRepository.RemoveHorseFromSamuraiAsync(samuraiId);
+            return NoContent();
+        }
     }
 }
