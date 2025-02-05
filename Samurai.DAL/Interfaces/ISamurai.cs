486a8906baa6585fe999333bd5c9da9341a95a47
Nolfi <sourcebans.nolfi@gmail.com>
2025-02-05 11:12:04 +0100

    Updated interface

--- Samurai.DAL/Interfaces/ISamurai.cs
+++ Samurai.DAL/Interfaces/ISamurai.cs
@@ -4,5 +4,16 @@ namespace Samurai.DAL.Interfaces
 {
     public interface ISamurai
     {
+        Task AddSamuraiAsync(SamuraiModel samurai);
+        Task DeleteSamuraiAsync(int id);
+        Task<SamuraiModel> GetSamuraiAsync(int id);
+        Task<List<SamuraiModel>> GetAllSamuraisAsync();
+        Task UpdateSamuraiAsync(SamuraiModel samurai);
+        Task<List<SamuraiModel>> GetSamuraisWithHorseAsync();
+        Task<List<SamuraiModel>> GetSamuraisWithBattlesAsync();
+        Task AddBattleToSamuraiAsync(int samuraiId, int battleId);
+        Task RemoveBattleFromSamuraiAsync(int samuraiId, int battleId);
+        Task AddHorseToSamuraiAsync(int samuraiId, int horseId);
+        Task RemoveHorseFromSamuraiAsync(int samuraiId);
     }
 }
